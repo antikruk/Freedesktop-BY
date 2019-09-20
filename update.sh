b7455387b3b2dfb475993099fc3d1a4a8036417f
Zmicer <zmicerturok@gmail.com>
2019-09-20 14:17:00 +0300

    2

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ dir1=sources/accounts-service
 dir2=sources/appstream-glib
 dir3=sources/pulseaudio
 dir4=sources/xdg-user-dirs
-
+dir5=sources/polkit-gnome-1
 
 
 
@@ -15,7 +15,7 @@ url1=https://gitlab.freedesktop.org/accountsservice/accountsservice/raw/master/p
 url2=https://github.com/hughsie/appstream-glib/raw/master/po/en_GB.po
 url3=https://l10n.gnome.org/POT/PulseAudio.master/PulseAudio.master.be.po
 url4=https://l10n.gnome.org/POT/xdg-user-dirs.master/xdg-user-dirs.master.be.po
-
+url5=https://l10n.gnome.org/module/po/polkit/master/po/be.po
 
 
 
@@ -27,5 +27,6 @@ wget -N -P $dir1 $url1
 wget -N -P $dir2 $url2
 wget -N -P $dir3 $url3
 wget -N -P $dir4 $url4
+wget -N -P $dir5 $url5
 
 exit
